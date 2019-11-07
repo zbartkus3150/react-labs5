a32ec394bae1a1ba730478fb9d792607fb1d0407
zbartkus <zbartkuss3150@gmail.com>
2019-11-07 12:42:03 +0100

    Lab5Ex4

--- src/PlayerTwo.js
+++ src/PlayerTwo.js
@@ -1,17 +1,26 @@
 import React from 'react'
 
 class PlayerTwo extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            counter: 0
+        };
+    }
+
     render(){
         return(
             <div className="row card">
                 <span><b>Player Two</b></span><br/>
                 <span>Name: {this.props.name}</span><br/>
-                <span>Played number of times: 
-                <label>4</label></span><br/>
+                <span>Played number of times: {this.state.counter}</span><br/>
                 <button
                     type="button"
                     onClick={() => {
                         this.props.btnClick(this.props.name);
+                        this.setState(oldstate => ({
+                            counter: oldstate.counter + 1
+                          }));
                     }}
                     disabled={this.props.playingNow ? "disabled" : ""}
                     >
